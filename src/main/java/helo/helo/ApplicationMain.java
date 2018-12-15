f4b34d47b99aad74a04737ae94de2eca51a64556
radiodio <shoichi.tsukamoto@gmail.com>
2018-12-16 02:37:27 +0900

    snmp4j sample

--- src/main/java/helo/helo/ApplicationMain.java
+++ src/main/java/helo/helo/ApplicationMain.java
@@ -1,8 +1,12 @@
 package helo.helo;
 
+import helo.helo.snmp.TrapSenderVersion1;
+
 public class ApplicationMain {
     public static void main(String[] args){
         System.out.print("helo");
 
+        TrapSenderVersion1 trapV1 = new TrapSenderVersion1();
+        trapV1.sendTrap_Version1();
     }
 }
